6b657de20a868f0fa69af2e99781a5a4a93b23aa
Romain Ruetschi <romain.ruetschi@gmail.com>
2019-07-02 16:12:11 +0200

    Update stainless to 0.3.0

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,13 +3,18 @@ import stainless.annotation._
 
 object Main {
 
-  def test = {
+  def valid = {
     1 + 1 == 2
+  }.holds
+
+  def invalid(n: BigInt) = {
+    n == 42
   }.holds
 
   @ignore
   def main(args: Array[String]): Unit = {
-    println(test)
+    println(valid)
+    println(invalid(1))
   }
 
 }
